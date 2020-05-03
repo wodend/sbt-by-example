a9fa7bb401a2aebd24a5006827417876eb3b4650
Weston Odend'hal <wodend@tuta.io>
2020-05-03 15:10:19 -0700

    Parse JSON using Play JSON.

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,5 +1,12 @@
 package example
 
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+
+import core.Weather
+
 object Hello extends App {
-  println("Hello")
+  val w = Await.result(Weather.weather, 10.seconds)
+  println(s"Hello! The weather in New York is $w.")
+  Weather.http.close()
 }
